d1d031934e9eb20304e6aa9e885fa3886ed9c3a3
Jana <jana.lasser@ds.mpg.de>
2022-03-03 17:13:04 +0100

    specified python 3

--- run_get_sampled_stream.sh
+++ run_get_sampled_stream.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 touch toggle
-python get_sampled_stream.py > get_sampled_stream_out.txt 2>get_sampled_stream_err.txt
-python notify.py
+python3 get_sampled_stream.py > get_sampled_stream_out.txt 2>get_sampled_stream_err.txt
+python3 notify.py
 rm toggle

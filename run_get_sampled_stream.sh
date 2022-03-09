533179c70fae6a3549ecf721b7c2dd36ef1cd4cf
Jana <jana.lasser@ds.mpg.de>
2022-03-09 11:35:33 +0100

    clean up scripts that are not needed anymore

--- run_get_sampled_stream.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-touch toggle
-python3 get_sampled_stream.py > get_sampled_stream_out.txt 2>get_sampled_stream_err.txt
-python3 notify.py
-rm toggle
--- watch_sampled_stream.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-FILE=toggle
-if [[ -f "$FILE" ]]; then
-    echo "$FILE exist"
-    exit
-fi
-
-./run_get_sampled_stream.sh

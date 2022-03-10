d8f30df2319675465dd64640355e6e1c8c364c28
Jana <jana.lasser@ds.mpg.de>
2022-03-10 18:32:09 +0100

    remove source files after sending over

--- code/send_data.sh
+++ code/send_data.sh
@@ -1,1 +1,1 @@
-rsync -avze ssh /data/twitter_sampled_stream_v2/ jlasser@medea:/data/twitter_sampled_stream_v2/tmp2/
+rsync --remove-source-files -avze ssh /data/twitter_sampled_stream_v2/ jlasser@medea:/data/twitter_sampled_stream_v2/tmp2/

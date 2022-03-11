fd18cf9df2a63cd426820a4ce4a6b3a417dd169b
jana@medea <jana.lasser@tugraz.at>
2022-03-11 17:47:30 +0100

    modified to run over all hours

--- code/parse_json_tweets.sh
+++ code/parse_json_tweets.sh
@@ -1,6 +1,15 @@
-for file in data/2022-03-10/16/*
+DAY="2022-03-11"
+#HOUR="1"
+
+for HOUR in 2 3 4 5 6 7 8 9 10 11 12 13 14 15
 do
-    echo "[]" | ./create_csv_head.jq > "${file%.json}.csv"
-    cat "$file" | ./parse_tweets.jq >> "${file%.json}.csv"
-    rm "$file"
+    for DIR in tmp1 tmp2
+    do
+        for file in /data/twitter_sampled_stream_v2/$DIR/$DAY/$HOUR/*.jsonl
+        do
+            echo "[]" | ./create_csv_head.jq > "${file%.jsonl}.csv"
+            cat "$file" | ./parse_tweets.jq >> "${file%.jsonl}.csv"
+            rm "$file"
+        done
+    done
 done

bb8ff5200392d60bc2cd924b2d156bfaaee569ed
Jana Lasser <jana.lasser@tugraz.at>
2022-03-12 09:52:43 +0100

    resolved conflict

--- code/send_data.sh
+++ code/send_data.sh
@@ -1,6 +1,2 @@
 #!/bin/bash
-<<<<<<< HEAD
 rsync --remove-source-files -avze ssh /data/twitter_sampled_stream_v2/ jlasser@medea:/data/twitter_sampled_stream_v2/tmp2/
-=======
-rsync --remove-source-files -avze ssh /data/twitter_sampled_stream_v2/ jlasser@medea:/data/twitter_sampled_stream_v2/tmp2/
->>>>>>> aad2d23fc80b3b2eda052308f85379130ae03f69

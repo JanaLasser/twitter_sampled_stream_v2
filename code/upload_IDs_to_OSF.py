5e1e8be19e79601f886e4d9c987752c14b08cd13
Jana <jana.lasser@ds.mpg.de>
2022-03-23 12:01:28 +0100

    added force override to update report hourly on OSF

--- code/upload_IDs_to_OSF.py
+++ code/upload_IDs_to_OSF.py
@@ -17,7 +17,6 @@ with open(join(cwd, "server_settings.txt"), 'r') as f:
 # file I/O paths and folder names
 src = server_settings["data_vault_dst"]
 prev_hour = datetime.datetime.today() - datetime.timedelta(hours=1)
-#prev_hour = datetime.datetime.strptime('2022-03-11 02:00:00', '%Y-%m-%d %H:%M:%S')
 yearmonthday = "{}-{:02d}-{:02d}"\
     .format(prev_hour.year, prev_hour.month, prev_hour.day)
 year = "{:04d}".format(prev_hour.year)
@@ -52,7 +51,7 @@ for fname in [fname_IDs, fname_report]:
     if os.path.exists(join(local_path, fname)):
         with open(join(local_path, fname), 'rb') as fp:
             try:
-                store.create_file(join(remote_path, fname), fp)
+                store.create_file(join(remote_path, fname), fp, force="f")
             except FileExistsError:
                 print(f"not writing {fname} because file exists at OSF")
     else:

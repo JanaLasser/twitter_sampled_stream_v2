937800eca3632cfd7cb50d1152e64e1db736f54b
Jana <jana.lasser@ds.mpg.de>
2022-03-23 16:38:36 +0100

    changed to read file paths from server_settings

--- code/parse_json_tweets.sh
+++ code/parse_json_tweets.sh
@@ -1,12 +1,14 @@
+source server_settings.txt
+
 DAY=$(date +%Y-%m-%d -d  "1 hour ago")
 HOUR=$(date +%H -d  "1 hour ago")
 
 for DIR in tmp1 tmp2
     do
-    for file in /data/twitter_sampled_stream_v2/$DIR/$DAY/$HOUR/*.jsonl
+    for file in $data_storage_dst/$DIR/$DAY/$HOUR/*.jsonl
     do
-        echo "[]" | /home/jlasser/twitter_sampled_stream_v2/code/create_csv_head.jq > "${file%.jsonl}.csv"
-        cat "$file" | /home/jlasser/twitter_sampled_stream_v2/code/parse_tweets.jq >> "${file%.jsonl}.csv"
+        echo "[]" | $repository_dst/code/create_csv_head.jq > "${file%.jsonl}.csv"
+        cat "$file" | $repository_dst/code/parse_tweets.jq >> "${file%.jsonl}.csv"
         rm "$file"
     done
 done

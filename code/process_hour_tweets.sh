99a21d2b0807cf820b6d5fce27fee41734fe449e
Jana <jana.lasser@ds.mpg.de>
2022-03-23 14:11:39 +0100

    fixed bug?

--- code/process_hour_tweets.sh
+++ code/process_hour_tweets.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 /home/jlasser/twitter_sampled_stream_v2/code/parse_json_tweets.sh
-/usr/local/anaconda3/bin/python /home/jlasser/twitter_sampled_stream_v2/code/collect_data.py
-/usr/local/anaconda3/bin/python /home/jlasser/twitter_sampled_stream_v2/code/collect_data.py
+/usr/local/anaconda3/bin/python /home/jlasser/twitter_sampled_stream_v2/code/collect_data.py /home/jlasser/twitter_sampled_stream_v2/code/
 /usr/local/anaconda3/bin/python /home/jlasser/twitter_sampled_stream_v2/code/upload_IDs_to_OSF.py /home/jlasser/twitter_sampled_stream_v2/code/
 

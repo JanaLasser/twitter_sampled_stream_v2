fb06a83dff1a8e658ac3034d97e08ec0b7821fd1
Jana <jana.lasser@ds.mpg.de>
2022-03-26 15:53:41 +0100

    added explicit credential file name

--- code/send_day_report.py
+++ code/send_day_report.py
@@ -2,10 +2,18 @@ import sampled_stream_functions as ssf
 import socket
 import datetime
 from os.path import join
+import sys
 
-
-credential_src = "/home/jlasser/twitter_sampled_stream_v2/code"
-data_src = "/data/twitter_sampled_stream_v2/"
+cwd = sys.argv[1] 
+server_settings = {}
+with open(join(cwd, "server_settings.txt"), 'r') as f:
+    for l in f:
+        server_settings[l.split('=')[0]] = l.split('=')[1].strip('\n')
+        
+email_credentials_dst = server_settings["EMAIL_CREDENTIALS_DST"]
+email_credentials_filename = server_settings["EMAIL_CREDENTIALS_FILENAME"]
+        
+data_src = server_settings["DATA_VAULT_DST"]
 host = socket.gethostname()
 
 yesterday = datetime.datetime.today() - datetime.timedelta(days=1)
@@ -18,5 +26,6 @@ with open(join(data_src, yesterday, f"{yesterday}_report.txt"), "r") as report_f
 ssf.notify(
     f"[NOTICE] daily report from {host}",
     body, 
-    credential_src=credential_src
+    credential_src=email_credentials_dst,
+    credential_fname=email_credentials_filename
 )

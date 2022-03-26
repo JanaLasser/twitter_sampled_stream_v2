c275574c9933230b7876f11ad565add06451e56c
Jana <jana.lasser@ds.mpg.de>
2022-03-26 15:52:08 +0100

    changed variable loading and names

--- code/parse_json_tweets.sh
+++ code/parse_json_tweets.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+cd /home/jlasser/twitter_sampled_stream_v2/code
 source server_settings.txt
 
 DAY=$(date +%Y-%m-%d -d  "1 hour ago")
@@ -5,10 +7,10 @@ HOUR=$(date +%H -d  "1 hour ago")
 
 for DIR in tmp1 tmp2
     do
-    for file in $data_storage_dst/$DIR/$DAY/$HOUR/*.jsonl
+    for file in $TMP_STORAGE_SERVER_1/$DIR/$DAY/$HOUR/*.jsonl
     do
-        echo "[]" | $repository_dst/code/create_csv_head.jq > "${file%.jsonl}.csv"
-        cat "$file" | $repository_dst/code/parse_tweets.jq >> "${file%.jsonl}.csv"
+        echo "[]" | ./create_csv_head.jq > "${file%.jsonl}.csv"
+        cat "$file" | ./parse_tweets.jq >> "${file%.jsonl}.csv"
         rm "$file"
     done
 done
--- code/parse_json_tweets_nonredundant.sh
+++ code/parse_json_tweets_nonredundant.sh
@@ -1,12 +1,14 @@
+#!/bin/bash
+cd /home/jlasser/twitter_sampled_stream_v2/code
 source server_settings.txt
 
 DAY=$(date +%Y-%m-%d -d  "1 hour ago")
 HOUR=$(date +%H -d  "1 hour ago")
 
-for file in $data_storage_dst/$DAY/$HOUR/*.jsonl
+for file in $TMP_STORAGE_SERVER_1/$DAY/$HOUR/*.jsonl
 do
-    echo "[]" | $repository_dst/code/create_csv_head.jq > "${file%.jsonl}.csv"
-    cat "$file" | $repository_dst/code/parse_tweets.jq >> "${file%.jsonl}.csv"
+    echo "[]" | ./create_csv_head.jq > "${file%.jsonl}.csv"
+    cat "$file" | ./parse_tweets.jq >> "${file%.jsonl}.csv"
     rm "$file"
 done
 

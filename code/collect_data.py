da557397eb5816357897eee25793de2795f9371e
Jana <jana.lasser@ds.mpg.de>
2022-03-26 16:47:58 +0100

    changed path handling to temporary folders

--- code/collect_data.py
+++ code/collect_data.py
@@ -12,26 +12,29 @@ server_settings = {}
 with open(join(cwd, "server_settings.txt"), 'r') as f:
     for l in f:
         server_settings[l.split('=')[0]] = l.split('=')[1].strip('\n')
-src = server_settings["DATA_VAULT_DST"]
+        
+src1 = server_settings["TMP_STORAGE_SERVER_1"]
+src2 = server_settings["TMP_STORAGE_SERVER_2"]
+dst = server_settings["DATA_VAULT_DST"]
 
 prev_hour = datetime.datetime.today() - datetime.timedelta(hours=1)
 day = "{}-{:02d}-{:02d}"\
     .format(prev_hour.year, prev_hour.month, prev_hour.day)
 hour = prev_hour.hour
 
-if not os.path.exists(join(src, day)):
+if not os.path.exists(join(dst, day)):
     # create folder for the day if it doesn't exist yet
-    os.mkdir(join(src, day))
+    os.mkdir(join(dst, day))
     
     # write header in the report file for the day
-    with open(join(src, day, f"{day}_report.txt"), "a") as report_file:
+    with open(join(dst, day, f"{day}_report.txt"), "a") as report_file:
         report_file.write(f"hour\tserv 1\tserv 2\ttotal\tdiff\tdiff %\n")
         
 
 # get the tweets for the given hour from both servers
 hourdirname = "{:02d}".format(hour)
-tmp1 = ssf.get_hour_files(join(src, "tmp1", day, hourdirname))
-tmp2 = ssf.get_hour_files(join(src, "tmp2", day, hourdirname))
+tmp1 = ssf.get_hour_files(join(src1, day, hourdirname))
+tmp2 = ssf.get_hour_files(join(src2, day, hourdirname))
 
 # Extract the tweet IDs from bot datasets and calculate the difference
 # Note: the difference between the two data sets can occur because
@@ -51,7 +54,7 @@ N = len(hour_tweets)
 
 # write the stats of the current hour into the report
 print(f"{hour}\t{len(ids1)}\t{len(ids2)}\t{N}\t{diff}\t{round(diff/N * 100, 2)}%")
-with open(join(src, day, f"{day}_report.txt"), "a") as report_file:
+with open(join(dst, day, f"{day}_report.txt"), "a") as report_file:
     report_file.write(f"{hour}\t{len(ids1)}\t{len(ids2)}\t{N}\t{diff}\t{round(diff/N * 100, 2)}%\n")
 
 # extract unique users, keep user stats from the most recent post in case there is 
@@ -62,15 +65,15 @@ hour_users = hour_tweets[ssf.AUTHOR_COLS + ["created_at"]]\
     .drop(columns=["created_at"])
 
 # save tweet IDs, unique users and tweets
-np.savetxt(join(src, day, "{}_{:02d}_IDs.txt.xz".format(day, hour)),
+np.savetxt(join(dst, day, "{}_{:02d}_IDs.txt.xz".format(day, hour)),
            hour_tweets["id"].values, fmt="%s")
 
-hour_users.to_csv(join(src, day, "{}_{:02d}_users.csv.xz".format(day, hour)),
+hour_users.to_csv(join(dst, day, "{}_{:02d}_users.csv.xz".format(day, hour)),
           compression="xz", index=False)
 
-hour_tweets.to_csv(join(src, day, "{}_{:02d}_tweets.csv.xz".format(day, hour)),
+hour_tweets.to_csv(join(dst, day, "{}_{:02d}_tweets.csv.xz".format(day, hour)),
           compression="xz", index=False)
 
 # clean up
-shutil.rmtree(join(src, "tmp1", day, hourdirname))
-shutil.rmtree(join(src, "tmp2", day, hourdirname))
+shutil.rmtree(join(src1, day, hourdirname))
+shutil.rmtree(join(src2, day, hourdirname))

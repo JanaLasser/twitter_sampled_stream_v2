4041792bb05b352ce951e030ce8804f823283f18
Jana <jana.lasser@ds.mpg.de>
2022-03-26 17:01:37 +0100

    new variables for tmp file paths

--- code/parse_json_tweets.sh
+++ code/parse_json_tweets.sh
@@ -1,17 +1,22 @@
 #!/bin/bash
-cd /home/jlasser/twitter_sampled_stream_v2/code
+cd /home/jlasser/twitter_sampled_stream_v2_test/code
 source server_settings.txt
 
 DAY=$(date +%Y-%m-%d -d  "1 hour ago")
 HOUR=$(date +%H -d  "1 hour ago")
 
-for DIR in tmp1 tmp2
-    do
-    for file in $TMP_STORAGE_SERVER_1/$DIR/$DAY/$HOUR/*.jsonl
-    do
-        echo "[]" | ./create_csv_head.jq > "${file%.jsonl}.csv"
-        cat "$file" | ./parse_tweets.jq >> "${file%.jsonl}.csv"
-        rm "$file"
-    done
+for file in $TMP_STORAGE_SERVER_1/$DAY/$HOUR/*.jsonl
+do
+    echo "[]" | ./create_csv_head.jq > "${file%.jsonl}.csv"
+    cat "$file" | ./parse_tweets.jq >> "${file%.jsonl}.csv"
+    rm "$file"
 done
+
+for file in $TMP_STORAGE_SERVER_2/$DAY/$HOUR/*.jsonl
+do
+    echo "[]" | ./create_csv_head.jq > "${file%.jsonl}.csv"
+    cat "$file" | ./parse_tweets.jq >> "${file%.jsonl}.csv"
+    rm "$file"
+done
+
 
